c3766e9e4cd937d554f68471e97a213cccf050ec
Sampo Kupiainen <sampokupiainen@gmail.com>
2022-02-24 16:37:26 +0200

    Update config.js

--- Osa 4/blogilista/utils/config.js
+++ Osa 4/blogilista/utils/config.js
@@ -1,5 +1,7 @@
-let PORT = 3003
-let MONGODB_URI = "mongodb+srv://sjjk:SALASANA@cluster0.8dsqy.mongodb.net/blogilista?retryWrites=true&w=majority"
+const PORT = 3003
+const MONGODB_URI = 'mongodb+srv://sjjk:SALASANA@cluster0.8dsqy.mongodb.net/blogilista?retryWrites=true&w=majority' === 'test' 
+  ? 'mongodb+srv://sjjk:SALASANA@cluster0.8dsqy.mongodb.net/blogilista_tests?retryWrites=true&w=majority'
+  : 'mongodb+srv://sjjk:SALASANA@cluster0.8dsqy.mongodb.net/blogilista?retryWrites=true&w=majority'
 
 module.exports = {
   MONGODB_URI,
