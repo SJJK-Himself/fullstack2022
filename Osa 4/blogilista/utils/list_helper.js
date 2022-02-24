d707d4f1ff3af2a513b0b626b22ef734d978ae75
Sampo Kupiainen <sampokupiainen@gmail.com>
2022-02-24 14:04:33 +0200

    4.4 done

--- Osa 4/blogilista/utils/list_helper.js
+++ Osa 4/blogilista/utils/list_helper.js
@@ -2,6 +2,12 @@ const dummy = (blogs) => {
     return 1
 }
   
+const totalLikes = (listWithOneBlog) => {
+    const result = listWithOneBlog.map(blog => blog.likes).reduce((prev, next) => (prev + next))
+    return result
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
