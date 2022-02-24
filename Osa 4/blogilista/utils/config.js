832a27f5e514d7dfb703d436c3dbaf99c3cfc5f7
Sampo Kupiainen <58609452+SJJK-Himself@users.noreply.github.com>
2022-02-24 17:04:37 +0200

    asd

--- Osa 4/blogilista/utils/config.js
+++ Osa 4/blogilista/utils/config.js
@@ -1,5 +1,5 @@
 const PORT = 3003
-const MONGODB_URI = 'mongodb+srv://sjjk:sjjk0090@cluster0.8dsqy.mongodb.net/blogilista?retryWrites=true&w=majority' === 'test' 
+const MONGODB_URI = 'mongodb+srv://sjjk:SALASANA@cluster0.8dsqy.mongodb.net/blogilista?retryWrites=true&w=majority' === 'test' 
   ? 'mongodb+srv://sjjk:SALASANA@cluster0.8dsqy.mongodb.net/blogilista_tests?retryWrites=true&w=majority'
   : 'mongodb+srv://sjjk:SALASANA@cluster0.8dsqy.mongodb.net/blogilista?retryWrites=true&w=majority'
 
