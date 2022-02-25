1d136e79d9d239e9f45068aa14217fb4af11c53c
Sampo Kupiainen <sampokupiainen@gmail.com>
2022-02-25 18:05:26 +0200

    4.17 done

--- Osa 4/blogilista/controllers/blogs.js
+++ Osa 4/blogilista/controllers/blogs.js
@@ -1,8 +1,11 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog
+    .find({}).populate('user', { username: 1, name: 1 })
+
   response.json(blogs)
 })
 
@@ -16,15 +19,24 @@ blogsRouter.get('/:id', async (request, response) => {
   }
 })
 
-blogsRouter.post('/', (request, response) => {
-    const blog = new Blog(request.body)
-    console.log("New blog posted")
-  
-    blog
-      .save()
-      .then(result => {
-        response.status(201).json(result)
-      })
+blogsRouter.post('/', async (request, response, next) => {
+  const body = request.body
+
+  const user = await User.findById(body.userId)
+
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+    user: user._id
+  })
+
+  const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
+  response.json(savedBlog)
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
