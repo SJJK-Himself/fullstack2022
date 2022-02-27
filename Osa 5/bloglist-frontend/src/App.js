753aa9f59f3bc2a0cfb2252ed21abc1c8b1dff6e
Sampo Kupiainen <sampokupiainen@gmail.com>
2022-02-27 13:59:13 +0200

    5.5 done

--- Osa 5/bloglist-frontend/src/App.js
+++ Osa 5/bloglist-frontend/src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login'
+import BlogForm from './components/BlogForm'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
@@ -11,6 +12,8 @@ const App = () => {
   const [username, setUsername] = useState('') 
   const [password, setPassword] = useState('') 
   const [user, setUser] = useState(null)
+
+  const [blogFormVisible, setBlogFormVisible] = useState(false)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -93,33 +96,30 @@ const App = () => {
     </form>      
   )
 
-  const blogForm = () => (
-    <form onSubmit={addBlog}>
-      <h3>Add a blog</h3>
-      Title:
-        <input
-          type="text"
-          value={title}
-          name="title"
-          onChange={({ target }) => setTitle(target.value)}
-        />
-      Author:
-        <input
-          type="text"
-          value={author}
-          name="author"
-          onChange={({ target }) => setAuthor(target.value)}
-        />
-      Url:
-        <input
-          type="text"
-          value={url}
-          name="url"
-          onChange={({ target }) => setUrl(target.value)}
-        />
-      <button type="submit">Save blog</button>
-    </form>  
-  )
+  const blogForm = () => {
+    const hideWhenVisible = { display: blogFormVisible ? 'none' : '' }
+    const showWhenVisible = { display: blogFormVisible ? '' : 'none' }
+
+    return (
+      <div>
+        <div style={hideWhenVisible}>
+          <button onClick={() => setBlogFormVisible(true)}>Add a blog</button>
+        </div>
+        <div style={showWhenVisible}>
+          <BlogForm
+            title={title}
+            author={author}
+            url={url}
+            handleTitleChange={({ target }) => setTitle(target.value)}
+            handleAuthorChange={({ target }) => setAuthor(target.value)}
+            handleUrlChange={({ target }) => setUrl(target.value)}
+            addBlog={addBlog}
+          />
+          <button onClick={() => setBlogFormVisible(false)}>Cancel</button>
+        </div>
+      </div>
+    )
+  }
 
   return (
     <div>
---
+++ Osa 5/bloglist-frontend/src/components/BlogForm.js
@@ -0,0 +1,39 @@
+const BlogForm = ({
+    addBlog,
+    title,
+    author,
+    url,
+    handleTitleChange,
+    handleAuthorChange,
+    handleUrlChange
+  }) => {
+  return (
+    <form onSubmit={addBlog}>
+      <h3>Add a blog</h3>
+      Title:
+        <input
+          type="text"
+          value={title}
+          name="title"
+          onChange={handleTitleChange}
+        />
+      Author:
+        <input
+          type="text"
+          value={author}
+          name="author"
+          onChange={handleAuthorChange}
+        />
+      Url:
+        <input
+          type="text"
+          value={url}
+          name="url"
+          onChange={handleUrlChange}
+        />
+      <button type="submit">Save blog</button>
+    </form>  
+    )
+  }
+
+export default BlogForm
